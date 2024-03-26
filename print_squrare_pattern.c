9e1cf7c4240f41844e101e9799ec6be88a5a30fc
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-26 09:28:22 +0000

    Tue Mar 26 09:28:22 AM UTC 2024

---
+++ print_squrare_pattern.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+  int n;
+  scanf("%d", &n);
+  // Complete the code to print the pattern.
+  return 0;
+}
