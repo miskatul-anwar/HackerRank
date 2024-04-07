b056b6300569197e9120b33a2b64ad391537079a
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-07 15:41:11 +0600

    Sun Apr  7 03:41:11 PM +06 2024

--- CPP/arrays_intro.cpp
+++ CPP/arrays_intro.cpp
@@ -1,16 +1,20 @@
 #include "bits/stdc++.h"
+#include <iostream>
 using namespace std;
 
 int main(void) {
   int n;
   cin >> n;
   vector<int> array;
-  for (auto i : array) {
-    cin >> i;
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+    array.push_back(val);
   }
   reverse(array.begin(), array.end());
   for (auto i : array) {
-    cout << i;
+    cout << i << " ";
   }
+  cout << endl;
   return 0;
 }
